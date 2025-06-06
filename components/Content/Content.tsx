cc32464f2fd9130098f51b12e57bb97b93c198b1
SE-Josh <jokezxcv@gmail.com>
2025-06-06 12:01:11 +0800

    feat: 新增專案描述及圖片輪播功能，可用於 Skills

--- components/Content/Content.tsx
+++ components/Content/Content.tsx
@@ -18,8 +18,37 @@ export default function Content({ activeTab }: ContentProps) {
       )}
       {activeTab === "projects" && (
         <div className="flex flex-col items-center gap-2">
-          <Image src={windowIcon} alt="projects" width={32} height={32} />
-          <p className="text-center">Projects coming soon.</p>
+          <div className="p-5">
+            <div className="carousel w-full">
+              <div id="item1" className="carousel-item w-full flex flex-col">
+                <p>這是第一個專案</p>
+                <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp" className="w-full" />
+              </div>
+              <div id="item2" className="carousel-item w-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp" className="w-full" />
+              </div>
+              <div id="item3" className="carousel-item w-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp" className="w-full" />
+              </div>
+              <div id="item4" className="carousel-item w-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp" className="w-full" />
+              </div>
+            </div>
+            <div className="flex w-full justify-center gap-2 py-2">
+              <a href="#item1" className="btn btn-xs">
+                1
+              </a>
+              <a href="#item2" className="btn btn-xs">
+                2
+              </a>
+              <a href="#item3" className="btn btn-xs">
+                3
+              </a>
+              <a href="#item4" className="btn btn-xs">
+                4
+              </a>
+            </div>
+          </div>
         </div>
       )}
       {activeTab === "contact" && (
