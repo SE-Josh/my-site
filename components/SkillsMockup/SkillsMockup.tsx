562447bac43f36baaf067511fa6e56e5ff78960a
Josh <59156522+SE-Josh@users.noreply.github.com>
2025-06-07 15:55:58 +0800

    update SkillsMockup categories

--- components/SkillsMockup/SkillsMockup.tsx
+++ components/SkillsMockup/SkillsMockup.tsx
@@ -7,13 +7,28 @@ const SkillsMockup: FC = () => {
         <code>{`const skillSet = {`}</code>
       </pre>
       <pre data-prefix="&gt;" className="text-primary">
-        <code>{`  languages: ['TypeScript', 'Python']`}</code>
+        <code>{`  solutions: ['IoT', 'AI']`}</code>
       </pre>
       <pre data-prefix="&gt;" className="text-secondary">
-        <code>{`  frameworks: ['Next.js', 'React']`}</code>
+        <code>{`  messaging: ['MQTT', 'Kafka']`}</code>
       </pre>
       <pre data-prefix="&gt;" className="text-accent">
-        <code>{`  tools: ['Docker', 'GitHub']`}</code>
+        <code>{`  aiot: ['EdgeX', 'Azure IoT']`}</code>
+      </pre>
+      <pre data-prefix="&gt;" className="text-info">
+        <code>{`  security: ['OAuth2', 'JWT']`}</code>
+      </pre>
+      <pre data-prefix="&gt;" className="text-success">
+        <code>{`  cloud: ['AWS', 'Azure']`}</code>
+      </pre>
+      <pre data-prefix="&gt;" className="text-warning">
+        <code>{`  documentation: ['Markdown', 'Swagger']`}</code>
+      </pre>
+      <pre data-prefix="&gt;" className="text-info">
+        <code>{`  workflow: ['Jira', 'Trello']`}</code>
+      </pre>
+      <pre data-prefix="&gt;" className="text-success">
+        <code>{`  ciCd: ['GitHub Actions', 'GitLab CI']`}</code>
       </pre>
       <pre data-prefix="$">
         <code>{`};`}</code>
