085a4659ecfda1bbebb10c3c4b9792228695924b
Josh <jokezxcv@gmail.com>
2025-07-18 14:15:44 +0800

    調整 AboutTab 組件的樣式，改善響應式顯示效果

--- components/Content/AboutTab.tsx
+++ components/Content/AboutTab.tsx
@@ -8,14 +8,14 @@ export default function AboutTab() {
 
   return (
     <main className="flex flex-col items-center justify-center w-full font-mono">
-      <section className="flex flex-col items-start justify-start relative text-left w-full max-w-[375px] group">
-        <div className="select-none font-bold text-3xl leading-[60px] mb-2 transition-all duration-1500 group-hover:pl-[50%] group-hover:text-transparent md:group-hover:pl-[30%]">
+      <section className="flex flex-col items-start justify-start relative text-left w-full max-w-[220px] md:max-w-[375px] group">
+        <div className="select-none font-bold text-3xl leading-[60px] mb-2 transition-all duration-1500 md:group-hover:pl-[50%] group-hover:text-transparent md:group-hover:pl-[30%]">
           {greeting}
         </div>
-        <p className="select-none absolute text-[#29282866] font-extrabold text-3xl leading-[35px] mt-[41px] ml-[30px] transition-colors duration-1000 group-hover:text-primary">
+        <p className="select-none absolute text-[#29282866] font-extrabold text-3xl leading-[35px] mt-[41px] ml-[30px] transition-colors duration-1000 group-hover:text-primary invisible md:visible">
           {shadow}
         </p>
-        <p className="select-none text-7xl leading-[60px] font-bold self-end mb-8 transition-all duration-1500 group-hover:pr-[40%] group-hover:text-primary">
+        <p className="select-none text-7xl leading-[60px] font-bold self-end mb-8 transition-all duration-1500 md:group-hover:pr-[40%] group-hover:text-primary">
           {name}
           <span className="text-[#e2b714]">{dot}</span>
         </p>
